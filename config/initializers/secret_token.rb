ce78faa9e34cda66833797b3dfc225c38584d7b4
Justin Kenyon <kenyonj@gmail.com>
2014-03-24 23:06:06 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FlavorList::Application.config.secret_key_base = '21a4d16a1f5494260f629c825927de01a748010a52e46dbd60cea2f5b5afffaf0da1a37b24239b210c42ba495feb15e574cb5f09f4b118b167a13f06266c5b01'
